5712502faa69b59c6b54a70a0370a5bf4054507b
htu311 <123193993+htu311@users.noreply.github.com>
2024-02-28 18:46:15 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,14 +23,14 @@ def main():
 
     # Create a form for user input
     st.subheader("Enter Data")
-    Date, Description ,Amount ,Comments = st.columns([1, 16])
-    with Date:
+    col1, col2 ,col3 ,col4 = st.columns([1, 16])
+    with col1:
         column1_data = st.text_input("Date", "")
-    with Description:
+    with col2:
         column2_data = st.text_input("Description", "")
-    with Amount:
+    with col3:
         column3_data = st.text_input("Amount", "")
-    with Comments:
+    with col4:
         column3_data = st.text_input("Comments", "")
    
     # Create a button to submit the data
