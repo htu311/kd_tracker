d7533edd2b62bdb991d4dd18d1f4c66923de31e8
htu311 <123193993+htu311@users.noreply.github.com>
2024-02-28 18:32:43 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 import requests
 from urllib.error import URLError
-st.title('Veterniatry')
-
+st.title('🐟🐦🐍🐢🐹🐰🐷🐮🐑🐴 Veterinary Clinic 🐶🐱🐭🐾🐧🐘🦒🐨🐼🐒')
 st.header('Vet Services')
 st.text('🥗 Home Visit')
 st.text('🥗 General Health Check up')
